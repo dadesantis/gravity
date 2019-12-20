a16954db5e71d2333e2aae1d030b49d1cb440fed
Dominic <dadesantis96@yahoo.com>
2019-12-20 01:40:26 -0500

    style cleanup

--- sketch.js
+++ sketch.js
@@ -10,7 +10,6 @@ var canvasHeight = 850;
 var maxPlanetSize = 200;
 
 function setup() {
-
 	/* Create world and info canvases*/
 	info = createGraphics(windowWidth, windowHeight*.3);
 	info.background('white');
@@ -22,16 +21,12 @@ function setup() {
 	world.style('margin', 'auto');
 	world.style('display', 'block');
 	
-
 	/* Scene Objects */
 	body_1 = new Body(random(maxPlanetSize, (canvasWidth-maxPlanetSize)), random(maxPlanetSize, (canvasHeight-maxPlanetSize)), floor(random(150, 400)), floor(random(200, 400)));
 	body_2 = new Body(random(maxPlanetSize, (canvasWidth-maxPlanetSize)), random(maxPlanetSize, (canvasHeight-maxPlanetSize)), floor(random(25, 100)), floor(random(50, 100)));
 
 	bodies.push(body_1);
 	bodies.push(body_2);
-
-	show_body(bodies[0]);
-
 }
 
 function draw() {
@@ -50,16 +45,6 @@ function draw_world() {
 	body_2.update();
 
 	getLiveStats(bodies[0]);
-}
-
-function clicked(body) {
-	d = floor(dist(body.pos.x, body.pos.y, mouseX, mouseY));
-	if (d < body.diameter / 2) {
-		console.log(body + " was clicked!");
-		return true;
-	} else {
-		return false;
-	}
 }
 
 /**
@@ -82,4 +67,14 @@ function show_body(body) {
 	info.textSize(20);
 	info.fill(body.color);
 	info.ellipse(info.width*.065, info.height/1.75, body.diameter*view_scale);
+}
+
+function clicked(body) {
+	d = floor(dist(body.pos.x, body.pos.y, mouseX, mouseY));
+	if (d < body.diameter / 2) {
+		console.log(body + " was clicked!");
+		return true;
+	} else {
+		return false;
+	}
 }
